d4437c790c8055187216be100ed94cd1c24dbf72
Your Name <you@example.com>
2025-02-17 18:02:30 +0800

    update about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 import { CloudArrowUpIcon, ServerIcon } from '@heroicons/react/20/solid'
-import { IconBrandTailwind } from '@tabler/icons-react'
+import { IconBrandTailwind,IconBrandGithub,IconBrandTwitter,IconBrandTelegram,IconBrandDiscord,IconNote } from '@tabler/icons-react'
 
 export default function Example() {
   return (
@@ -26,50 +26,52 @@ export default function Example() {
           <div className="lg:pr-4">
             <div className="max-w-xl text-base leading-7 text-gray-700 dark:text-gray-100 lg:max-w-lg">
               <p>
-                Born from the need for a simple, yet powerful explorer that anyone can deploy, customize, and extend, <strong>Block Kube X</strong> leverages the robustness of Next.js, React, and TailwindCSS for a seamless user experience, paired with MongoDB for efficient data management.
+              Pump.fun, one of the most successful products on Solana, sees daily trading volumes in the hundreds of millions, thousands of new coin launches, and hundreds of thousands of active addresses. While many trading tools have emerged around Pump.fun, we found a lack of financial tools—especially basic lending products.
               </p>
               <ul role="list" className="mt-8 space-y-8 text-gray-600 dark:text-gray-300">
                 <li className="flex gap-x-3">
-                  <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-sky-600 dark:text-sky-300" aria-hidden="true" />
+                  {/* <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-sky-600 dark:text-sky-300" aria-hidden="true" /> */}
                   <span>
-                    <strong className="font-semibold text-gray-900 dark:text-gray-200">Easy to deploy.</strong> Leverage Next.js and Docker for a straightforward deployment process. Simplify your workflow with push-to-deploy features.
+                    <strong className="font-semibold text-gray-900 dark:text-gray-200">Max Buy : </strong> 
+                     Deposit SOL and instantly see the maximum amount of memecoins you can purchase, without manually adjusting leverage or complex parameters.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  <IconBrandTailwind className="mt-1 h-5 w-5 flex-none text-sky-600 dark:text-sky-300" aria-hidden="true" />
+                  {/* <IconBrandTailwind className="mt-1 h-5 w-5 flex-none text-sky-600 dark:text-sky-300" aria-hidden="true" /> */}
                   <span>
-                    <strong className="font-semibold text-gray-900 dark:text-gray-200">Powerfull Style.</strong> Customize with Tailwind CSS for rapid UI development. Design your explorer with minimal effort.
+                    <strong className="font-semibold text-gray-900 dark:text-gray-200">Max Borrow : </strong> Easily access additional funds to capitalize on market opportunities, all through a streamlined borrowing process.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
-                  <ServerIcon className="mt-1 h-5 w-5 flex-none text-sky-600 dark:text-sky-300" aria-hidden="true" />
+                  {/* <ServerIcon className="mt-1 h-5 w-5 flex-none text-sky-600 dark:text-sky-300" aria-hidden="true" /> */}
                   <span>
-                    <strong className="font-semibold text-gray-900 dark:text-gray-200">MongoDB.</strong> Utilize MongoDB for efficient data storage and retrieval. Ensure scalable and reliable database operations.
+                    <strong className="font-semibold text-gray-900 dark:text-gray-200"> Earn SOL : </strong> Put your assets to work through a simple, low-friction staking mechanism, allowing you to earn passive returns.
                   </span>
                 </li>
               </ul>
-              <p className="mt-8">
-                 As part of the larger <strong>Block Kube</strong> project—an initiative aimed at simplifying blockchain network management on Kubernetes—<strong>Block Kube X</strong> stands as a testament to our commitment to making blockchain technology more accessible and manageable. 
-              </p>
-              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-300">The Block Kube.</h2>
-              <p className="mt-6">
-                <strong>Block Kube</strong> itself is a pioneering tool that facilitates the setup, configuration, and management of blockchain networks, supporting both private and public environments with a user-friendly CLI.
-              </p>
-              <p className="mt-6">
-                This project is for anyone looking to delve into the Ethereum blockchain, whether for educational purposes, development, or enterprise use.
-              </p>
 
               <p className="mt-6">
-                Explore <strong>Block Kube X</strong> on GitHub: <a href="https://github.com/marcus-gomes-v/BlockKube-X" className="text-blue-500 hover:text-blue-600">Block Kube X Repository</a> or see it in action here: <a href="https://blockkube-x.vercel.app/" className="text-blue-500 hover:text-blue-600">Live Demo</a>.
-              </p>
+                Join us : <br/>
+                <div style={{display:'flex'}}>
+                  <a href='https://github.com/pumplend'>
+                    <IconBrandGithub></IconBrandGithub>
+                  </a>
+                  <a href='https://x.com/pumpmaxfun'>
+                    <IconBrandTwitter></IconBrandTwitter>
+                  </a>
+                  <a href='https://t.me/+50XGEOayi543NzM1'>
+                    <IconBrandTelegram></IconBrandTelegram>
+                  </a>
+                  <a href='https://discord.gg/X6BdtCRM3W'>
+                    <IconBrandDiscord></IconBrandDiscord>
+                  </a>
+                  <a href='https://docs.pumpmax.fun/'>
+                    <IconNote></IconNote>
+                  </a>
+                </div>
 
-              <p className="mt-6">
-                Join us in our journey to build a simple, easy-to-use, and configurable explorer.
               </p>
               
-              <p className="mt-6">
-                Your contributions can help make <strong>Block Kube X</strong> even better for everyone in the blockchain community.
-              </p>
             </div>
           </div>
         </div>
