0cfdd5d54880e6e14edc89f5abdbd79c5d818865
Your Name <you@example.com>
2025-02-18 17:32:28 +0800

    now try to change it into client side only

--- src/app/position/[data]/page.tsx
+++ src/app/position/[data]/page.tsx
@@ -1,30 +1,15 @@
 
 'use client';
-
-import BlockDetail from '@/app/components/details/block/block-detail';
-
 import PositionDetails from '@/app/components/details/positions/position-details';
 import React, { useEffect, useState } from 'react';
-
-
 export default function TransactionPage({ params }: { params: { data: string } }) {
   const [block, setBlock] = useState<any>(null);
   const { data } = params; 
 
   useEffect(() => {
     if (!data) return;
-    // const fetchBlockData = async () => {
-    //   const response = await fetch(`/api/block/${data}`);
-    //   if (!response.ok) {
-    //     console.error('Failed to fetch transactions data');
-    //     return;
-    //   }
-    //   let blockData = await response.json();
-    //   setBlock(blockData);
-    // };
-    // fetchBlockData();
     setBlock(data);
-  }, [data]); // Re-run the effect if the hash changes
+  }, [data]); 
 
   if (!block) {
     return <div>Loading...</div>;
